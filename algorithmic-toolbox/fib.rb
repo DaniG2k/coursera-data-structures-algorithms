067e2277bd05909f71ade5d7ea901222bed36b0b
Daniele Pestilli <dpestilli@funnelback.com>
2016-07-20 15:09:44 +0100

    Code cleanup

--- algorithmic-toolbox/fib.rb
+++ algorithmic-toolbox/fib.rb
@@ -1,13 +1,8 @@
 # Uses ruby
 def calc_fib(n)
   return n if n <= 1
-  n -= 1
   a, b = 0, 1
-  n.times do
-    tmp = a
-    a = b
-    b = tmp + a
-  end
+  (n-1).times { tmp = a; a = b; b = tmp + a }
   b
 end
 
