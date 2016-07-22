1475a11983b7f6dd8c0c69b23cad33c4092b7b6c
Daniele Pestilli <dpestilli@funnelback.com>
2016-07-22 09:46:52 +0100

    Added fibonacci_huge.rb

--- algorithmic-toolbox/introduction/fibonacci_huge.rb
+++ algorithmic-toolbox/introduction/fibonacci_huge.rb
@@ -1,6 +1,13 @@
 # Uses ruby
-def get_fibonaccihuge(n, m)
+
+def get_fibonacci_huge(n, m)
+  modulos, i = [0, 1], 0
+  until i > 0 && modulos[i] == 0 && modulos[i+1] == 1
+    modulos.push(((modulos[i] % m) + (modulos[i+1] % m)) % m)
+    i += 1
+  end
+  modulos[n % i]
 end
 
-puts get_fibonaccihuge(281621358815590, 30524)
-
+a, b = gets.split.map {|num| num.to_i}
+puts get_fibonacci_huge(a, b)
